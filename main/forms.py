34286e55b15cd9204cc1718bdb0b4971fd94bc97
unknown <muhammadtoychiboyev1@gmail.com>
2023-11-09 11:44:31 +0500

    new me templates

--- main/forms.py
+++ main/forms.py
@@ -1,37 +1,37 @@
-from .models import Contact
-from django import forms
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs.update({
-            "class": "form-control",
-            "id": "name",
-            "type": "text",
-            "placeholder": "Your Name",
-            "name": "name"
-        })
-        self.fields['email'].widget.attrs.update({
-            "class": "form-control",
-            "id": "email",
-            "type": "email",
-            "name": "email",
-            "placeholder": "Your Email",
-        })
-        self.fields['phone_number'].widget.attrs.update({
-            "class": "form-control",
-            "type": "number",
-            "name": "phone",
-            "placeholder": "Phone number",
-        })
-        self.fields['message'].widget.attrs.update({
-            "class": "form-control",
-            "name": "message",
-            "rows": 5,
-            "placeholder": "Message",
-        })
+# from .models import Contact
+# from django import forms
+#
+#
+# class ContactForm(forms.ModelForm):
+#     class Meta:
+#         model = Contact
+#         fields = '__all__'
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['name'].widget.attrs.update({
+#             "class": "form-control",
+#             "id": "name",
+#             "type": "text",
+#             "placeholder": "Your Name",
+#             "name": "name"
+#         })
+#         self.fields['email'].widget.attrs.update({
+#             "class": "form-control",
+#             "id": "email",
+#             "type": "email",
+#             "name": "email",
+#             "placeholder": "Your Email",
+#         })
+#         self.fields['phone_number'].widget.attrs.update({
+#             "class": "form-control",
+#             "type": "number",
+#             "name": "phone",
+#             "placeholder": "Phone number",
+#         })
+#         self.fields['message'].widget.attrs.update({
+#             "class": "form-control",
+#             "name": "message",
+#             "rows": 5,
+#             "placeholder": "Message",
+#         })
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import index, about, feedback, contact, book, book_detail
+# from .views import index, about, feedback, contact, book, book_detail
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    path('feedback/', feedback, name='feedback'),
-    path('book/', book, name='book'),
-    path('book/<int:pk>/', book_detail, name='book-detail'),
+    # path('', index, name='index'),
+    # path('about/', about, name='about'),
+    # path('contact/', contact, name='contact'),
+    # path('feedback/', feedback, name='feedback'),
+    # path('book/', book, name='book'),
+    # path('book/<int:pk>/', book_detail, name='book-detail'),
 ]
